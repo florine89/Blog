03d4c1e23dcb56e00a8bc3a4ff5e067ffb643f21
florine89 <ferreiraflorine@gmail.com>
2022-11-04 10:23:37 +0100

    etape 2 ok

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
+const router = require('./router.js');
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-console.log('bonjour');
-
-app.get('/', function(req, res) {
-    res.send('hello world');
-  });
+app.use(express.static(__dirname + '/static'));
 
 
 
 
 
+
+
+
+app.use(router);
 
 const port = 3011;
 app.listen(port, () => {
---
+++ router.js
@@ -0,0 +1,18 @@
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(request, response) => {
+   response.render('index')
+  });
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
